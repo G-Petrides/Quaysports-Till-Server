00dfc3df60f62e23c5ebf351a2808d23ee3ce4c6
G-Petrides <geoff.petrides@gmail.com>
2022-08-17 09:12:21 +0100

    Logging bug fix

--- TS/server-modules/log.ts
+++ TS/server-modules/log.ts
@@ -1,6 +1,5 @@
 export const log = (path:string, log:any = {})=> {
     let timestamp = new Date()
-    if(typeof log === "object" && log._id) delete log._id
     let line = {date:timestamp.toJSON(), path:path, log:log}
     console.dir(line,{depth:3,colors:true})
 }

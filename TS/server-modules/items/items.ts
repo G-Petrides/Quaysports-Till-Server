b71c3910c95ee097f5e087bae8555e58d79f2b4f
G-Petrides <geoff.petrides@gmail.com>
2022-08-22 14:23:02 +0100

    Updated image bug fix

--- TS/server-modules/items/items.ts
+++ TS/server-modules/items/items.ts
@@ -9,14 +9,14 @@ export const getImages = async (sku:string, type:string) => {
     }
     const item = await mongoI.findOne<itemImageDetails>("Items", {SKU: sku}, {IMAGES: 1})
     if (!item) return
-    if (!item.IMAGES) return
+    if (!item.IMAGES || item.IMAGES === {}) return
     if (type) {
         let path = item.IMAGES[type].link ? "/images/" + item.IMAGES[type].link + "/" : "/images/" + sku + "/"
         return path + item.IMAGES[type].filename
     } else {
         let arr = []
         for (let i in item.IMAGES) {
-            let path = item.IMAGES[i].link ? "/images/" + item.IMAGES[i].link + "/" : "/images/" + sku + "/"
+            let path = item.IMAGES[i]?.link ? "/images/" + item.IMAGES[i].link + "/" : "/images/" + sku + "/"
             arr.push(path + item.IMAGES[i].filename)
         }
         return arr

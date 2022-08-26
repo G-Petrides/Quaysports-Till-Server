c6359e159b35d0e83c852d5907bae9a781949d62
G-Petrides <geoff.petrides@gmail.com>
2022-08-26 12:13:21 +0100

    Tracked down and fixed Image find bug that was crashing server

--- TS/server-modules/items/items.ts
+++ TS/server-modules/items/items.ts
@@ -8,18 +8,10 @@ export const getImages = async (sku:string, type:string) => {
         }
     }
     const item = await mongoI.findOne<itemImageDetails>("Items", {SKU: sku}, {IMAGES: 1})
-    if (!item) return
-    if (!item.IMAGES || item.IMAGES === {}) return
+    if (!item || !item.IMAGES || !item.IMAGES[type]) return
     if (type) {
         let path = item.IMAGES[type].link ? "/images/" + item.IMAGES[type].link + "/" : "/images/" + sku + "/"
         return path + item.IMAGES[type].filename
-    } else {
-        let arr = []
-        for (let i in item.IMAGES) {
-            let path = item.IMAGES[i]?.link ? "/images/" + item.IMAGES[i].link + "/" : "/images/" + sku + "/"
-            arr.push(path + item.IMAGES[i].filename)
-        }
-        return arr
     }
 }
 
